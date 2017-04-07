92373cb1e1b9bfba270448c7147c9466863ffc7f
Alexander Leontyev <sleontjev@yandex.ru>
2017-04-07 22:51:38 +0300

    Update pom.xml: replace Thymeleaf with Freemarker

--- src/main/java/com/imagestudio/ImagestudioCrmApplication.java
+++ src/main/java/com/imagestudio/ImagestudioCrmApplication.java
@@ -1,11 +1,13 @@
 package com.imagestudio;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class})
 public class ImagestudioCrmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ImagestudioCrmApplication.class, args);
 	}
