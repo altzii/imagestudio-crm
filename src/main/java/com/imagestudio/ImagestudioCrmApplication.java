9a65ecbc91bf59f919cb03336ca2875bd8295323
Alexander Leontyev <sleontjev@yandex.ru>
2017-04-07 21:35:13 +0300

    Initialize project

---
+++ src/main/java/com/imagestudio/ImagestudioCrmApplication.java
@@ -0,0 +1,12 @@
+package com.imagestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagestudioCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagestudioCrmApplication.class, args);
+	}
+}
